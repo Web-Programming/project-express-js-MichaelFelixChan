87b3d2def1d884a4b2e459c7b5c468ddecd18ea8
MichaelFelixChan <michaelfelixchandra_2327240030@mhs.mdp.ac.id>
2025-09-26 15:02:53 +0700

    materi express generator

---
+++ project-express-js-MichaelFelixChandra-main/aplikasi_3/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port=3000;
+
+app.use(express.static('public'));
+
+app.listen(port, (req, res) => {
+    console.log(`Server Running at http://localhost:${port}`);
+});
